dddde84a5f1808eb8cac15932bfe4d2f90af37b2
Fabio Boa Sorte Ribeiro <fabioboasorte@gmail.com>
2026-06-29 08:42:30 -0300

    migrations

---
+++ Migrations/20260625134742_Inicial.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LojaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Marca = table.Column<string>(type: "TEXT", nullable: false),
+                    SKU = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoriaProduto",
+                columns: table => new
+                {
+                    CategoriasId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProdutosId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoriaProduto", x => new { x.CategoriasId, x.ProdutosId });
+                    table.ForeignKey(
+                        name: "FK_CategoriaProduto_Categorias_CategoriasId",
+                        column: x => x.CategoriasId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoriaProduto_Produtos_ProdutosId",
+                        column: x => x.ProdutosId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "Informática" },
+                    { 2, "Periféricos" },
+                    { 3, "Notebooks" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Marca", "Nome", "Preco", "SKU" },
+                values: new object[,]
+                {
+                    { 1, "Dell", "Notebook", 3500m, "SKNOTE11909" },
+                    { 2, "Logitech", "Mouse", 150m, "SKMOUSE1190119" },
+                    { 3, "Logitech", "Teclado", 350m, "SKKEYBRD1122909" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CategoriaProduto",
+                columns: new[] { "CategoriasId", "ProdutosId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 },
+                    { 2, 3 },
+                    { 3, 1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoriaProduto_ProdutosId",
+                table: "CategoriaProduto",
+                column: "ProdutosId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoriaProduto");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
